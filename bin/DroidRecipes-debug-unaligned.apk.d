914840af5dfb1e1ba0edca8a80a49b8157e34723
Stelios Almpanis <sa@navarino.gr>
2011-12-16 11:14:37 +0200

    remove of bin

--- bin/DroidRecipes-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/programming/androids/DroidRecipes/bin/DroidRecipes-debug-unaligned.apk : \
-/home/programming/androids/DroidRecipes/bin/DroidRecipes.ap_ \
-/home/programming/androids/DroidRecipes/bin/classes.dex \
--- bin/DroidRecipes.ap_.d
+++
@@ -1,9 +0,0 @@
-/home/programming/androids/DroidRecipes/bin/DroidRecipes.ap_ \
- : /home/programming/androids/DroidRecipes/res/layout/main.xml \
-/home/programming/androids/DroidRecipes/res/layout/employee_list_item.xml \
-/home/programming/androids/DroidRecipes/res/menu/menu.xml \
-/home/programming/androids/DroidRecipes/res/drawable/icon.png \
-/home/programming/androids/DroidRecipes/res/values/strings.xml \
-/home/programming/androids/DroidRecipes/bin/res/drawable/icon.png \
-/home/programming/androids/DroidRecipes/assets/navaroni.db \
-/home/programming/androids/DroidRecipes/AndroidManifest.xml \
